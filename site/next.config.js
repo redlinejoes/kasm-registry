951404f1a9248e9e81eac071bacb85f65cc1c9ff
Joseph Young <3170654+redlinejoes@users.noreply.github.com>
2023-06-10 22:21:40 -0400

    update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,13 +4,13 @@ const nextConfig = {
   env: {
     name: 'Young Security Kasm Registry',
     description: 'The Young Security store for Kasm supported workspaces.',
-    icon: 'https://redlinejoes.github.io/kasm-registry/1.0/favicon.ico',
+    icon: 'https://redlinejoes.github.io/kasm-registry/favicon.ico',
     listUrl: 'https://redlinejoes.github.io/kasm-registry/',
     contactUrl: 'https://github.com/redlinejoes/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/1.0',
+  basePath: '/kasm-registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
